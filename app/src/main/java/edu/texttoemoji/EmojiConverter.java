fed4ee93ed1dc2cf12e03b4987c9f2fcaa7fb938
Ankit Maurya <ankit.1410025@kiet.edu>
2017-11-21 02:07:59 +0530

    Library1.07

--- app/src/main/java/edu/texttoemoji/EmojiConverter.java
+++ app/src/main/java/edu/texttoemoji/EmojiConverter.java
@@ -80,10 +80,9 @@ public class EmojiConverter extends AppCompatEditText implements DashboardView {
         ArrayList<String> rawText = new ArrayList<>();
         word = s.toString().split(" ");
         Collections.addAll(rawText, word);
-        if (list != null) {
             String processedText = emojiPresenter.processString(list, rawText);
             this.setText(processedText);
-        }
+        
 
     }
 
